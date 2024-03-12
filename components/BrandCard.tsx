751c5b330c890600691376ca514fbe03aa36fc4f
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-12 19:58:25 +0100

    jkh

--- components/BrandCard.tsx
+++ components/BrandCard.tsx
@@ -28,7 +28,7 @@ const BrandCard = ({ id, imgUrl, title, index, active, handleClick }) => (
           className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
         >
           <img
-            src="/headset.svg"
+            src="/VCC1.png"
             alt="headset"
             className="w-1/2 h-1/2 object-contain"
           />
