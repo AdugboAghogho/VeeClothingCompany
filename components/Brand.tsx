b64178546ca107ae8e31d14003f3883395890076
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-12 20:13:33 +0100

    ,hjkz

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-
+import '../components/Brand.css'
 import styles from '../app/index';
 import { staggerContainer } from './../utils/motion';
 import { brands } from '@/constants/data';
@@ -13,18 +13,18 @@ const Explore = () => {
   const [active, setActive] = useState('world-2');
 
   return (
-    <section className={`${styles.paddings}`} id='explore'> 
+    <section className={`${styles.paddings} dd  bg-blue-100`} id='explore'> 
       <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} mx-auto flex flex-col`}
+        className={`${styles.innerWidth} mx-auto flex flex-col `}
       >
         {/* <TypingText title="| The World" textStyles="text-center" />
         <TitleText title={<>Choose the World you want <br className='md:block hidden' /> to explore</>} textStyles="text-center" /> */}
 
-        <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
+        <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5 '>
           {brands.map((world, index) => (
             <BrandCard
               key={world.id}
