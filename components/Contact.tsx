fc2d51cfc96bdd0f5162feda369bf354ea2fdd32
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-13 14:20:45 +0100

    lkjhg

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,71 +1,61 @@
 import React from 'react'
 import './Contact.css'
+import { FaFacebookF, FaPinterestP } from "react-icons/fa";
+import { FaInstagram, FaXTwitter } from "react-icons/fa6";
+import { IoMdLock, IoMdMail } from "react-icons/io";
+import CustomButton from './CustomButton';
+import Link from 'next/link';
 
 const Contact = () => {
   return (
     <div className='the'>
        <div className="container">
             <div className="forms-container">
-            <div className="signin-signup">
-                <form action="#" className="sign-in-form">
-                <h2 className="title">Contact Us</h2>
-                <div className="input-field">
-                    <i className="fas fa-user"></i>
-                    <input type="text" placeholder="Username" />
+                <div className="signin-signup">
+                    <form action="#" className="sign-in-form">
+                        <h2 className="title">Contact Us</h2>
+
+                        <input className="input-field1" type="text" placeholder="Name" required/>
+
+                        <input className="input-field1" type="text" placeholder="LastName" required/>
+                        
+
+                        <div className="input-field">
+                            <IoMdMail className='icon' />
+                            <input type="gmail" placeholder="Mail" required/>
+                        </div>
+
+                        
+                        <textarea name='message' className="input-field" rows={10} placeholder="Message" required></textarea>
+                        
+
+                        <CustomButton 
+                            title="Send Message"
+                            containerStyles='bg-primary-blue text-white rounded-full left-[15px] mt-12  h-[50px] w-full tracking-[3px]  hover:bg-blue-900'
+                            rightIcon='/right-arrow.svg'
+                        />
+
+                        <p className="social-text">Or Sign in with social platforms</p>
+
+                        <div className="social-media">
+                            <Link href="#" className="social-icon">
+                              <FaFacebookF />
+                            </Link>
+
+                            <Link href="#" className="social-icon">
+                              <FaXTwitter />
+                            </Link>
+
+                            <Link href="#" className="social-icon">
+                              <FaInstagram />
+                            </Link>
+
+                            <Link href="#" className="social-icon">
+                              <FaPinterestP />
+                            </Link>
+                        </div>
+                    </form>
                 </div>
-                <div className="input-field">
-                    <i className="fas fa-lock"></i>
-                    <input type="password" placeholder="Password" />
-                </div>
-                <input type="submit" value="Login" className="btn solid" />
-                <p className="social-text">Or Sign in with social platforms</p>
-                <div className="social-media">
-                    <a href="#" className="social-icon">
-                    <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="#" className="social-icon">
-                    <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="#" className="social-icon">
-                    <i className="fab fa-google"></i>
-                    </a>
-                    <a href="#" className="social-icon">
-                    <i className="fab fa-linkedin-in"></i>
-                    </a>
-                </div>
-                </form>
-                {/* <form action="#" className="sign-up-form">
-                <h2 className="title">Sign up</h2>
-                <div className="input-field">
-                    <i className="fas fa-user"></i>
-                    <input type="text" placeholder="Username" />
-                </div>
-                <div className="input-field">
-                    <i className="fas fa-envelope"></i>
-                    <input type="email" placeholder="Email" />
-                </div>
-                <div className="input-field">
-                    <i className="fas fa-lock"></i>
-                    <input type="password" placeholder="Password" />
-                </div>
-                <input type="submit" className="btn" value="Sign up" />
-                <p className="social-text">Or Sign up with social platforms</p>
-                <div className="social-media">
-                    <a href="#" className="social-icon">
-                    <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="#" className="social-icon">
-                    <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="#" className="social-icon">
-                    <i className="fab fa-google"></i>
-                    </a>
-                    <a href="#" className="social-icon">
-                    <i className="fab fa-linkedin-in"></i>
-                    </a>
-                </div>
-                </form> */}
-            </div>
             </div>
 
             
