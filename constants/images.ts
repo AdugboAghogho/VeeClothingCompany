3ef8aaafdd75fb0d95107f1f8d2e412265d77c9d
adugbo aghogho <adugboaghogho@gmail.com>
2024-03-17 22:54:10 +0100

    thgh

--- constants/images.ts
+++ constants/images.ts
@@ -3,18 +3,24 @@ import cop1 from '../public/cop01.jpg';
 import cop2 from '../public/cop02.png';
 import cop3 from '../public/cop03.png';
 import cop4 from '../public/cop04.png';
-import cop5 from '../public/cop05.png';
+import cop5 from '../public/cop05.jpg';
+import cop6 from '../public/cop06.jpg';
+import cop7 from '../public/cop07.jpg';
 import kaftan01 from '../public/kaftan01.jpg';
 import kaftan02 from '../public/kaftan02.jpg';
 import kaftan03 from '../public/kaftan03.jpg';
-import kaftan04 from '../public/kaftan04.jpg';
+import kaftan04 from '../public/kaftan04.jpeg';
 import kaftan05 from '../public/kaftan05.jpg';
-import kaftan06 from '../public/katfan06.png';
+import kaftan06 from '../public/kaftan06.jpeg';
+import kaftan07 from '../public/kaftan07.jpeg';
+import kaftan08 from '../public/kaftan08.png';
+import kaftan09 from '../public/kaftan09.png';
+import kaftan10 from '../public/kaftan10.png';
 import suit01 from '../public/suit01.jpg';
 import suit02 from '../public/suit02.png';
 import suit03 from '../public/suit03.png';
 import suit04 from '../public/suit04.jpg';
-import hero from '../public/hero.png';
+import suit05 from '../public/suit05.jpeg';
 import hero1 from '../public/hero1.jpeg';
 import hero2 from '../public/hero2.png';
 import brand01 from '../public/brand01.png';
@@ -32,17 +38,23 @@ export default {
   cop3,
   cop4,
   cop5,
+  cop6,
+  cop7,
   kaftan01,
   kaftan02,
   kaftan03,
   kaftan04,
   kaftan05,
   kaftan06,
+  kaftan07,
+  kaftan08,
+  kaftan09,
+  kaftan10,
   suit01,
   suit02,
   suit03,
   suit04,
-  hero,
+  suit05,
   hero1,
   hero2,
   brand01,
