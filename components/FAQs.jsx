f2c1e9b7771d5fb4ee30b19245b3b2255552fcb2
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-02 09:40:09 +0100

    uy

--- components/FAQs.jsx
+++ components/FAQs.jsx
@@ -1,69 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './FAQs.css';
 import { Asks } from './../constants/data';
 import { FaMinus, FaPlus } from 'react-icons/fa';
 
 const FAQs = () => {
-  const accordinoItems = document.querySelectorAll('.accordino-item');
+  const [expandedIndexes, setExpandedIndexes] = useState([]);
 
-accordinoItems.forEach(item => {
-  const title = item.querySelector('.accordino-title');
-  const content = item.querySelector('.accordino-content');
+  const handleFAQToggle = (index) => {
+    setExpandedIndexes((prevIndexes) =>
+      prevIndexes.includes(index)
+        ? prevIndexes.filter((i) => i !== index)
+        : [...prevIndexes, index]
+    );
+  };
 
-  title.addEventListener('click', () => {
-    accordinoItems.forEach(otherItem => {
-      if (otherItem !== item) {
-        otherItem.classList.remove('active');
-      } else {
-        item.classList.toggle('active');
-      }
-    });
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- return (
-    <div className=" faqs flex__center bg-blue-100">
+  return (
+    <div className="faqs flex__center bg-blue-100">
       <h1 className="hero__title pl-5">Faq</h1>
-      <div className="flex-center  faqs__container">
+      <div className="flex-center faqs__container">
         {Asks.map((Ask, index) => (
-          <article key={index} className="faq">
-            <div className="faq__icon">
-              <FaPlus className='uil uil-plus'/>
+          <article key={index} className="faq h-20">
+            <div
+              className="faq__icon"
+              onClick={() => handleFAQToggle(index)}
+            >
+              {expandedIndexes.includes(index) ? (
+                <FaMinus className="uil uil-minus" />
+              ) : (
+                <FaPlus className="uil uil-plus" />
+              )}
             </div>
-
             <div className="question__answer">
-              <h4 className='accordino-title'>{Ask.title}</h4>
-              <p className="hero__subtitle accordino-content" >{Ask.name}</p>
+              <h4 className="accordino-title">{Ask.title}</h4>
+              <p
+                className={`hero__subtitle accordino-content ${
+                  expandedIndexes.includes(index) ? 'show' : ''
+                }`}
+              >
+                {Ask.name}
+              </p>
             </div>
-              <FaMinus className='uil uil-minus' />
           </article>
         ))}
       </div>
     </div>
-  ); 
+  );
 };
 
 export default FAQs;
