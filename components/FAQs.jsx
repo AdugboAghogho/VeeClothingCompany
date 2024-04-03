40759c21e374f406c6f88abb9988d63fbce85ec7
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-03 10:37:31 +0100

    lkj

--- components/FAQs.jsx
+++ components/FAQs.jsx
@@ -7,7 +7,8 @@ import Accordion from './Accordion';
 const FAQs = () => {
 
 
-  
+  s
+
  return (
     <div className=" faqs flex__center bg-blue-100">
       <h1 className="hero__title p-10">FAQs</h1>
