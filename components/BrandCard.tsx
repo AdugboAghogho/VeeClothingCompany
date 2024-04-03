dc892d74d376d58ebdcade7b315187a3750100b8
adugbo aghogho <adugboaghogho@gmail.com>
2024-04-03 10:56:40 +0100

    vee

--- components/BrandCard.tsx
+++ components/BrandCard.tsx
@@ -38,7 +38,7 @@ const BrandCard: React.FC<BrandCardProps> = ({ id, imgUrl, title, index, active,
           />
         </div>
         {/* <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
-          Enter Metaverse
+          Enter Metaverse 
         </p> */}
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
           {title}
